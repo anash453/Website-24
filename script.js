16fc4d655a8c60b8ae81694080a6a05c4f5bfee8
anash453 <anash6291@gmail.com>
2025-01-31 17:47:35 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// https://preview.themeforest.net/item/maxshop-premium-magento-2-19-store-theme/full_screen_preview/9897734?_ga=2.35583906.2052785564.1738261510-993370187.1728848196
 let myBtn = document.getElementById("myBtn");
 
 window.onscroll = function () {
